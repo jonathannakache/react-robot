ddc85bb2d5e945e2cd7a3dc36dd47fd03c1c4fe4
Jonathan Nakache <john.nakache@gmail.com>
2020-02-25 14:28:06 +0200

    Updates

---
+++ precache-manifest.75716687c5c93356f521dc567b75155d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5d4acb78ba6e133eaa40f25f39babd",
+    "url": "/react-robot/index.html"
+  },
+  {
+    "revision": "4753e11757d733e4e448",
+    "url": "/react-robot/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "b79c1ac17bd7bb6b8eb3",
+    "url": "/react-robot/static/css/main.f76b2253.chunk.css"
+  },
+  {
+    "revision": "4753e11757d733e4e448",
+    "url": "/react-robot/static/js/2.ddc36304.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-robot/static/js/2.ddc36304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b79c1ac17bd7bb6b8eb3",
+    "url": "/react-robot/static/js/main.693e56fc.chunk.js"
+  },
+  {
+    "revision": "18ff55517736f0bd2705",
+    "url": "/react-robot/static/js/runtime-main.cc0556c6.js"
+  }
+]);
